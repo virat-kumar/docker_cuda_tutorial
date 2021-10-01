964f330d3cdf98abe1f0e4d7f9f35104f23be66e
vk001716 <vk001716@gmail.com>
2021-10-01 23:28:18 +0530

    Added getcwd to flask app

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 import flask
 import dlib
+import os
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
+print("current working directory is ", os.getcwd())
 dlib_gpu_result = str(dlib.DLIB_USE_CUDA)
 @app.route('/', methods=['GET'])
 def home():

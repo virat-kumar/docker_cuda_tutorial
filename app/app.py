43ae638fd492154f592eefd2721c3d9dfc7eb3fa
Virat Kumar <36535270+vk001716@users.noreply.github.com>
2021-10-01 23:51:37 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,10 +3,11 @@ import flask
 import os
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+pwd= os.getcwd()
 print("current working directory is ", os.getcwd())
 # dlib_gpu_result = str(dlib.DLIB_USE_CUDA)
 @app.route('/', methods=['GET'])
 def home():
-    return f"<h1>Flask app running</h1>"
+    return f"<h1>Flask app running present working directory is {pwd}</h1>"
 
 app.run(host = "0.0.0.0")

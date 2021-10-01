115f88ca3c5683ad391dcdfd605c5904201d312c
Virat Kumar <36535270+vk001716@users.noreply.github.com>
2021-10-01 23:42:41 +0530

    removed DLIB dependency

--- app/app.py
+++ app/app.py
@@ -1,12 +1,12 @@
 import flask
-import dlib
+# import dlib
 import os
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 print("current working directory is ", os.getcwd())
-dlib_gpu_result = str(dlib.DLIB_USE_CUDA)
+# dlib_gpu_result = str(dlib.DLIB_USE_CUDA)
 @app.route('/', methods=['GET'])
 def home():
-    return f"<h1>{dlib_gpu_result}</h1>"
+    return f"<h1>Flask app running</h1>"
 
 app.run(host = "0.0.0.0")

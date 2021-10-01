373dbb515ba5c9b5e523a142ff9e516c515ebe6e
Virat Kumar <36535270+vk001716@users.noreply.github.com>
2021-10-02 00:03:44 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,6 +8,6 @@ print("current working directory is ", os.getcwd())
 # dlib_gpu_result = str(dlib.DLIB_USE_CUDA)
 @app.route('/', methods=['GET'])
 def home():
-    return f"<h1>Flask app running present working directory is {pwd}</h1>"
+    return f"<h1>Flask app running present working directory is \{pwd}</h1>"
 
 app.run(host = "0.0.0.0")
